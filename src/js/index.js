1d52d2f05c420387a06c8ba03eae2b6458e3da21
EQ <167933418+ejquast@users.noreply.github.com>
2024-07-03 03:12:35 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -11,22 +12,28 @@ function SimpleCounter(props){
         <div className="calendar">
             <i className="fa-regular fa-clock"></i>
         </div>
-        <div className="four">{props.digitFour}</div>
-        <div className="three">{props.digitThree}</div>
-        <div className="two">{props.digitTwo}</div>
-        <div className="one">{props.digitOne}</div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
     </div>);
+}
+
+SimpleCounter.prototype = {
+    digitFour: propTypes.number,
+    digitThree: propTypes.number,
+    digitTwo: propTypes.number,
+    digitOne: propTypes.number
 }
 
 let counter = 0;
 setInterval(function(){
-    const four = Math.floor(counter/10000);
-    const three = Math.floor(counter/1000);
-    const two = Math.floor(counter/100);
-    const one = Math.floor(counter/10);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log(four,three,two,one);
+    counter++;
     //render your react application
-    ReactDOM.createRoot(document.getElementById('app')).render(<SimpleCounter digitOne={counter.indexOf()}/>);
+    ReactDOM.createRoot(document.getElementById('app')).render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>);
 },1000);
-
-
-
